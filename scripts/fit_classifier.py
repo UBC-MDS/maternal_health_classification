69ff46a99535ae51f80cc838cc6868e9133f1e45
JiaQuanLim <jiaquan280201@gmail.com>
2024-12-06 16:30:27 -0800

    fit and evaluate scripts ready to push

--- scripts/fit_classifier.py
+++ scripts/fit_classifier.py
@@ -3,8 +3,10 @@
 # date: 2024-12-05
 
 import pandas as pd
+import os
 import numpy as np
 import click
+import pickle
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, RandomizedSearchCV, cross_validate
 from sklearn.preprocessing import StandardScaler
@@ -21,12 +23,13 @@ from deepchecks.tabular.checks.data_integrity import FeatureFeatureCorrelation
 
 @click.command()
 @click.option('--training-data', type=str, help="Path to training data")
-@click.option('--preprocessor', type=str, help="Path to preprocessor object")
-@click.option('--pipeline-to', type=str, help="Path to directory where the pipeline object will be written to")
-#@click.option('--plot-to', type=str, help="Path to directory where the plot will be written to")
-#@click.option('--seed', type=int, help="Random seed", default=123)
+@click.option('--best_model_to', type=str, help="Path to directory where the best model object will be written to")
+@click.option('--tbl_to', type=str, help="Path to directory where the tables will be written to")
+@click.option('--seed', type=int, help="Random seed", default=111)
 
-def main(training_data, preprocessor, pipeline_to, plot_to, seed):
+def main(training_data, best_model_to, tbl_to, seed):
+    np.random.seed(seed)
+    
     # Read in training data and split into X and y
     train_df = pd.read_csv(training_data)
     X_train = train_df.drop(columns=["RiskLevel"])
@@ -37,20 +40,18 @@ def main(training_data, preprocessor, pipeline_to, plot_to, seed):
     check_feat_corr = FeatureFeatureCorrelation()
     check_feat_corr_result = check_feat_corr.run(maternal_train_ds)
     
-    # ------------------------------------------------------------------------------------
     # BASELINE MODELL: Dummy Classifier
     dummy_clf = DummyClassifier()
     scores = cross_validate(dummy_clf, X_train, y_train, cv=10, return_train_score=True)
     pd.DataFrame(scores)['test_score'].mean()
     
-    # ------------------------------------------------------------------------------------
     # Gaussian Bayes
     gaus_nb_pipe = make_pipeline(StandardScaler(), GaussianNB())
     gaus_nb_pipe.fit(X_train, y_train)
     cv_results = cross_validate(gaus_nb_pipe, X_train, y_train, return_train_score=True)
     results_df = pd.DataFrame(cv_results)
+    results_df.to_csv(os.path.join(tbl_to, "Gaussian_cv_scores.csv"), index=False)
     
-    # ------------------------------------------------------------------------------------
     # Decision Tree, Logistic Regression, SVC
     models = {
         "Decision Tree": DecisionTreeClassifier(random_state=123),
@@ -96,7 +97,7 @@ def main(training_data, preprocessor, pipeline_to, plot_to, seed):
             clf_pipe, X_train, y_train, cv=10, return_train_score=True, error_score='raise'
         )
     results_df = pd.DataFrame(results_dict).T
-
+    results_df.to_csv(os.path.join(tbl_to, "dt_svm_lr_cv_scores.csv"), index=True)
 
     dt = DecisionTreeClassifier(random_state=123)
     
@@ -108,7 +109,8 @@ def main(training_data, preprocessor, pipeline_to, plot_to, seed):
     random_search = RandomizedSearchCV(dt, param_dist, n_iter=100, n_jobs=-1, return_train_score = True, random_state=123)
     random_search.fit(X_train, y_train)
 
-
+    with open(os.path.join(best_model_to, "dt_tuned_fit.pickle"), 'wb') as f:
+        pickle.dump(random_search, f)
 
 if __name__ == '__main__':
     main()
